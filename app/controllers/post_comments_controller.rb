dfba19fe5ad107b2f6dd0d232772c54276522013
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-02-25 10:10:07 +0000

    [Finish]bookers-debug[postcomment]

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,7 +3,7 @@ class PostCommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
     comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
+    comment.book_id = book
     comment.save
     redirect_back(fallback_location: root_path)
   end

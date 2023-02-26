0f637593d3ecfebd7fe1b130b210402739e30c51
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-02-26 05:17:23 +0000

    [Add]bookers2-debug[follow]

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,13 +4,13 @@ class RelationshipsController < ApplicationController
  def create
    follower = current_user.relationships.build(followed_id: params[:user_id])
    follower.save
-   redirect_to request.refereer || root_path
+   redirect_to request.referrer || root_path
  end
  
  def destroy
-    follower = current_user.relationships.find_by(followed_id: params[:user_id])
+   follower = current_user.relationships.find_by(followed_id: params[:user_id])
    follower.destroy
-   redirect_to request.refereer || root_path
+   redirect_to request.referrer || root_path
  end
  
 end

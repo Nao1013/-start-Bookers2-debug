52a00711c56ca9fba3ba0f8cbdb2eb02d9b6ac28
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-02-27 01:25:21 +0000

    検索機能実装(未確認)

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "/saerch", to: "saerchs#saerch"
   devise_for :users
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]

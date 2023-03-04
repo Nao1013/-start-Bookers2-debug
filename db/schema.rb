fb810e8a03df617e92f728db56195a6b4399cdb7
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-03-04 07:53:24 +0000

    [Update]bookers2-debug[chat]

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_03_050957) do
+ActiveRecord::Schema.define(version: 2023_03_04_074532) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2023_03_03_050957) do
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "room_id"
   end
 
   create_table "favorites", force: :cascade do |t|

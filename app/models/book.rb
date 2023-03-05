589ad865fbf24bbf3148e3b193d43163f0f7a79a
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-03-05 13:02:27 +0000

    [修正]bookers2-7a[rank]

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,15 +5,14 @@ class Book < ApplicationRecord
   has_many :favorites_users, through: :favorites, source: :user
   has_many :post_comments, dependent: :destroy
   
+  # いいねランキング
+  has_many :week_favorites, -> { wehere(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
+  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
   
    def favorited_by?(user)
      self.favorites.exists?(user_id: user.id)
    end
-   
-  # def self.last_week
-    # Book.joins(:favorites).where(favorites: {created_at: 0.days.ago.prev_week..0.days.ago.prev_week(:sunday)}).group(:id).order("count(*) desc")
-  # end
    
 end

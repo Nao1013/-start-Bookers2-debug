fccd0b7d6f8f09987cf2ff5bf9ef201bcf35723b
Nao1013 <121780890+Nao1013@users.noreply.github.com>
2023-03-09 04:57:11 +0000

    [Add]投稿数検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index,:show,:edit,:update] do
+    get "search", to: "users#search"
     resource :relationships, only: [:create, :destroy]
       get 'followings' => 'relationships#followings', as: 'followings'
       get 'followers' => 'relationships#followers', as: 'followers'
